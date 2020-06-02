b5371062e2cc6daabd33d738f077c150eb823ddf
余文锦 <windpenguin@gmail.com>
2020-06-02 15:21:06 +0800

    [feat] 引用CMakeUtility

--- Distrib.bat
+++ Distrib.bat
@@ -3,6 +3,6 @@
 if exist Build rd /q /s Build
 mkdir Build
 cd Build
-cmake .. -DCALCULATOR_ROOT=E:/GitRepo/Calculator/Install
+cmake -DCALCULATOR_ROOT=E:/GitRepo/Calculator/Install -DCMAKE_UTILITY_ROOT=E:\GitRepo\CMakeUtility ..
 
 pause
